830e8298662c89381fd35bd23303529e65dfdb1a
hp <kyorigarurab@gmail.com>
2020-02-18 20:45:59 +0300

    brandonprogram

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +19,14 @@ EditText edit;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but= (Button)findViewById(R.id.button4);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,act3.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -27,5 +39,25 @@ EditText edit;
         message.setText("");
 
     }
+    public boolean onCreateOptionsMenu(Menu menuu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.huhuu, menuu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.music:
+                startActivity(new Intent(this, act1.class));
+                return true;
+            case R.id.yes:
+                startActivity(new Intent(this, act2.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 
 }
